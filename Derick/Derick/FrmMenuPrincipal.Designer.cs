0ba921c2688e67824f7d0010a128d30d43c48d28
E_ML <usuario@LAPTOP-ASUS-GAM>
2026-07-13 22:43:48 -0500

    LABEL

--- Derick/Derick/FrmMenuPrincipal.Designer.cs
+++ Derick/Derick/FrmMenuPrincipal.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Derick
+namespace Derick
 {
     partial class FrmMenuPrincipal
     {
@@ -51,8 +51,7 @@
             pbusuario = new PictureBox();
             panel1 = new Panel();
             label1 = new Label();
-            button1 = new Button();
-            button2 = new Button();
+            label2 = new Label();
             panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
@@ -315,8 +314,7 @@
             // panel1
             // 
             panel1.BackColor = Color.White;
-            panel1.Controls.Add(button2);
-            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(pnlcontenido);
             panel1.Controls.Add(panel2);
@@ -330,29 +328,21 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(566, 247);
+            label1.Location = new Point(620, 233);
             label1.Name = "label1";
-            label1.Size = new Size(279, 36);
+            label1.Size = new Size(110, 36);
             label1.TabIndex = 2;
-            label1.Text = "AHHHHHHHHHHH";
+            label1.Text = "holaaa";
             // 
-            // button1
+            // label2
             // 
-            button1.Location = new Point(568, 360);
-            button1.Name = "button1";
-            button1.Size = new Size(277, 79);
-            button1.TabIndex = 3;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(431, 96);
-            button2.Name = "button2";
-            button2.Size = new Size(183, 92);
-            button2.TabIndex = 4;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(586, 316);
+            label2.Name = "label2";
+            label2.Size = new Size(181, 32);
+            label2.TabIndex = 3;
+            label2.Text = "AHHHHHHH";
             // 
             // FrmMenuPrincipal
             // 
@@ -412,7 +402,6 @@
         private Panel panel1;
         private Label lblusuario;
         private Label label1;
-        private Button button1;
-        private Button button2;
+        private Label label2;
     }
 }
