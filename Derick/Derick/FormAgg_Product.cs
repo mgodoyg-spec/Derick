8da0efe1d3c7eb9194c989bad0f9b466277e0f50
E_ML <usuario@LAPTOP-ASUS-GAM>
2026-08-01 21:59:13 -0500

    Nuevo formulario para agregar los productos

---
+++ Derick/Derick/FormAgg_Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Derick
+{
+    public partial class FormAgg_Product : Form
+    {
+        public FormAgg_Product()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
