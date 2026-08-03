ce5dfd7ac79a1cfb049eb32cd5eaec228c4d53ae
DARIANNE\Usuario <molinadarianne59@gmail.com>
2026-08-02 23:08:17 -0500

    Diseño Nueva Ventas y Consultar Ventas

---
+++ Derick/Derick/FrmConsultarVentas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Derick
+{
+    public partial class FrmConsultarVentas : Form
+    {
+        public FrmConsultarVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvListado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnRegresarC_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("¿Desea cancelar? Se perderán los cambios realizados.", "Cancelar",
+    MessageBoxButtons.YesNo,
+    MessageBoxIcon.Question);
+
+            if (r == DialogResult.Yes)
+            {
+                frmVentas frm = this.Parent.Parent as frmVentas;
+                if (r != null)
+                {
+                    frm.MostrarFrm();
+                }
+            }
+        }
+    }
+}
---
+++ Derick/Derick/FrmNuevaVenta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Derick
+{
+    public partial class FrmNuevaVenta : Form
+    {
+        public FrmNuevaVenta()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCodigoVenta_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnGuardarV_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La venta se ha guardado correctamente.",
+                   "Registro de ventas",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Information);
+        }
+
+        private void btnCancelarV_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("La venta ha sido cancelada.",
+                    "Cancelar",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+        }
+
+        private void btnRegresarN_Click(object sender, EventArgs e)
+        {
+
+            DialogResult r = MessageBox.Show("¿Desea cancelar? Se perderán los cambios realizados.", "Cancelar",
+    MessageBoxButtons.YesNo,
+    MessageBoxIcon.Question);
+
+            if (r == DialogResult.Yes)
+            {
+                frmVentas frm = this.Parent.Parent as frmVentas;
+                if (frm != null)
+                {
+                    frm.MostrarFrm();
+                }
+            }
+        }
+    }
+}
